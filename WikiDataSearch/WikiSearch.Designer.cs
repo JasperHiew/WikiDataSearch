d00379feece3773a59a3e2f57f4bc9168e6a16c8
JasperHiew <jasperh15@gmail.com>
2022-03-22 09:08:33 +0800

    Add project files.

---
+++ WikiDataSearch/WikiSearch.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace WikiDataSearch
+{
+    partial class WikiSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxCategory = new System.Windows.Forms.TextBox();
+            this.textBoxStructure = new System.Windows.Forms.TextBox();
+            this.textBoxDefinition = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.listViewOutput = new System.Windows.Forms.ListView();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(13, 13);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 31);
+            this.buttonAdd.TabIndex = 0;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(13, 50);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(75, 32);
+            this.buttonEdit.TabIndex = 1;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(94, 13);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 32);
+            this.buttonDelete.TabIndex = 2;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(12, 126);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(136, 20);
+            this.textBoxName.TabIndex = 3;
+            // 
+            // textBoxCategory
+            // 
+            this.textBoxCategory.Location = new System.Drawing.Point(12, 171);
+            this.textBoxCategory.Name = "textBoxCategory";
+            this.textBoxCategory.Size = new System.Drawing.Size(136, 20);
+            this.textBoxCategory.TabIndex = 4;
+            // 
+            // textBoxStructure
+            // 
+            this.textBoxStructure.Location = new System.Drawing.Point(12, 214);
+            this.textBoxStructure.Name = "textBoxStructure";
+            this.textBoxStructure.Size = new System.Drawing.Size(136, 20);
+            this.textBoxStructure.TabIndex = 5;
+            // 
+            // textBoxDefinition
+            // 
+            this.textBoxDefinition.Location = new System.Drawing.Point(13, 264);
+            this.textBoxDefinition.Multiline = true;
+            this.textBoxDefinition.Name = "textBoxDefinition";
+            this.textBoxDefinition.Size = new System.Drawing.Size(178, 116);
+            this.textBoxDefinition.TabIndex = 6;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(374, 28);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(94, 36);
+            this.buttonSearch.TabIndex = 7;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(229, 37);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(122, 20);
+            this.textBoxInput.TabIndex = 8;
+            this.textBoxInput.TextChanged += new System.EventHandler(this.textBoxInput_TextChanged);
+            // 
+            // listViewOutput
+            // 
+            this.listViewOutput.HideSelection = false;
+            this.listViewOutput.Location = new System.Drawing.Point(229, 76);
+            this.listViewOutput.Name = "listViewOutput";
+            this.listViewOutput.Size = new System.Drawing.Size(239, 243);
+            this.listViewOutput.TabIndex = 9;
+            this.listViewOutput.UseCompatibleStateImageBehavior = false;
+            this.listViewOutput.SelectedIndexChanged += new System.EventHandler(this.listViewOutput_SelectedIndexChanged);
+            this.listViewOutput.Click += new System.EventHandler(this.listViewOutput_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(243, 343);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(85, 37);
+            this.buttonSave.TabIndex = 10;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(352, 343);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(85, 37);
+            this.buttonOpen.TabIndex = 11;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Category:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Structure:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 245);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Definition:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(229, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Search:";
+            // 
+            // WikiSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 392);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.listViewOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxDefinition);
+            this.Controls.Add(this.textBoxStructure);
+            this.Controls.Add(this.textBoxCategory);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonAdd);
+            this.Name = "WikiSearch";
+            this.Text = "WikiSearch";
+            this.Load += new System.EventHandler(this.WikiSearch_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxCategory;
+        private System.Windows.Forms.TextBox textBoxStructure;
+        private System.Windows.Forms.TextBox textBoxDefinition;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.ListView listViewOutput;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WikiDataSearch/WikiSearch.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace WikiDataSearch
+{
+    public partial class WikiSearch : Form
+    {
+        public WikiSearch()
+        {
+            InitializeComponent();
+        }
+        static int row = 10;
+        static int column = 3;
+        string[,] wikiArray = new string[row, column];
+        string defaultName = "Default.bin";
+
+        int currentRow = 0;
+
+        private void displayArray()
+        {
+            listViewOutput.Items.Clear();
+            listViewOutput.ForeColor = Color.Black;
+            for (int x = 0; x < row; x++)
+            {
+                ListViewItem lvi = new ListViewItem(wikiArray[x, 0]);
+                lvi.SubItems.Add(wikiArray[x, 1]);
+                lvi.SubItems.Add(wikiArray[x, 2]);
+                listViewOutput.Items.Add(lvi);
+            }
+        }
+
+        public void clearTextBox()
+        {
+            textBoxInput.Clear();
+            textBoxName.Clear();
+            textBoxCategory.Clear();
+            textBoxStructure.Clear();
+            textBoxDefinition.Clear();
+        }
+
+
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBoxName.Text) &&
+                !string.IsNullOrWhiteSpace(textBoxStructure.Text) &&
+                !string.IsNullOrWhiteSpace(textBoxCategory.Text) &&
+                !string.IsNullOrWhiteSpace(textBoxDefinition.Text))
+            {
+                for (int x = 0; x < row; x++)
+                {
+                    if (wikiArray[x, 0] == " ")
+                    {
+                        //wikiArray[x, 0] = textBoxInput.Text;
+                        wikiArray[x, 0] = textBoxName.Text;
+                        wikiArray[x, 1] = textBoxCategory.Text;
+                        wikiArray[x, 2] = textBoxStructure.Text;
+                        wikiArray[x, 3] = textBoxDefinition.Text;
+
+                        var result = MessageBox.Show("Would you like to add this information?", "Are you sure?",
+                            MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                        if (result == DialogResult.OK)
+                            break;
+                        else
+                        {
+
+                            wikiArray[x, 0] = "";
+                            wikiArray[x, 1] = "Category";
+                            wikiArray[x, 2] = "Structure";
+                            wikiArray[x, 3] = "Definition";
+                            break;
+                        }
+                    }
+                }
+            }
+            displayArray();
+            clearTextBox();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            // Reminder in case you want to change to listview instead of listbox
+            int data = listViewOutput.SelectedIndices[0];
+            if (data >= 0)
+            {
+                DialogResult result = MessageBox.Show("Would you like to delete this definition?",
+                    "Are you sure you want to delete this?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    // "" Emptying the textboxes
+                    wikiArray[data, 0] = "";
+                    wikiArray[data, 1] = "";
+                    wikiArray[data, 2] = "";
+                    wikiArray[data, 3] = "";
+                    displayArray();
+                    clearTextBox();
+                }
+            }
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            // Change to list view
+            int data = listViewOutput.SelectedIndices[0];
+            if (data >= 0)
+            {
+                var result = MessageBox.Show("Do you wish to edit this definition?"
+                    , "Finalise edit?", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                if (result == DialogResult.OK)
+                {
+                    wikiArray[data, 0] = textBoxName.Text;
+                    wikiArray[data, 1] = textBoxStructure.Text;
+                    wikiArray[data, 2] = textBoxCategory.Text;
+                    wikiArray[data, 3] = textBoxDefinition.Text;
+                    clearTextBox();
+
+                }
+            }
+        }
+
+
+        private void WikiSearch_Load(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialogVG = new OpenFileDialog();
+            openFileDialogVG.InitialDirectory = Application.StartupPath;
+            openFileDialogVG.Filter = "BIN Files|*.bin";
+            openFileDialogVG.Title = "Select a BIN File";
+            if (openFileDialogVG.ShowDialog() == DialogResult.OK)
+            {
+                openRecord(openFileDialogVG.FileName);
+            }
+            else
+            {
+                for(int x = 0; x < row; x++)
+                {
+                    wikiArray[x, 0] = " ";
+                    wikiArray[x, 1] = "~";
+                    wikiArray[x, 2] = " ";
+                } 
+            }
+            displayArray();
+        }
+
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialogVG = new OpenFileDialog();
+            openFileDialogVG.InitialDirectory = Application.StartupPath;
+            openFileDialogVG.Filter = "BIN Files|*.bin";
+            openFileDialogVG.Title = "Select a BIN File";
+            if (openFileDialogVG.ShowDialog() == DialogResult.OK)
+            {
+                openRecord(openFileDialogVG.FileName);
+            }
+        }
+        private void openRecord(string openFileName)
+        {
+            try
+            {
+                using (Stream stream = File.Open(openFileName, FileMode.Open))
+                {
+                    BinaryFormatter bin = new BinaryFormatter();
+                    for (int y = 0; y < column; y++)
+                    {
+                        for (int x = 0; x < row; x++)
+                        {
+                            wikiArray[x, y] = (string)bin.Deserialize(stream);
+                        }
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            displayArray();
+        }
+
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialogVG = new SaveFileDialog();
+            saveFileDialogVG.Filter = "bin file|*.bin";
+            saveFileDialogVG.Title = "Save a bin File";
+            saveFileDialogVG.InitialDirectory = Application.StartupPath;
+            saveFileDialogVG.DefaultExt = "bin";
+            saveFileDialogVG.ShowDialog();
+
+            string fileName = saveFileDialogVG.FileName;
+            if (saveFileDialogVG.FileName != "")
+            {
+                saveRecord(fileName);
+            }
+            else
+            {
+                saveRecord(defaultName);
+            }
+        }
+
+        private void saveRecord(string saveFileName)
+        {
+            try
+            {
+                using (Stream stream = File.Open(saveFileName, FileMode.Create))
+                {
+                    BinaryFormatter bin = new BinaryFormatter();
+                    for (int y = 0; y < column; y++)
+                    {
+                        for (int x = 0; x < row; x++)
+                        {
+                            bin.Serialize(stream, wikiArray[x, y]);
+                        }
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void listViewOutput_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listViewOutput_Click(object sender, EventArgs e)
+        {
+            int currentRecord = listViewOutput.SelectedIndices[0];
+            textBoxName.Text = wikiArray[currentRecord, 0];
+            textBoxCategory.Text = wikiArray[currentRecord, 1];
+            textBoxStructure.Text = wikiArray[currentRecord, 2];
+            //textBoxDefinition.Text = wikiArray[currentRecord, 3];
+        }
+
+        private void textBoxInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
